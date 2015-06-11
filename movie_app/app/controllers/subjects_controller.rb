a372e96865c7e2670a63ea8447f3fdfecf1baa65
Miguel Solano <m.a.solano93@gmail.com>
2015-06-11 02:19:23 -0500

    paperclip subject image addition

--- movie_app/app/controllers/subjects_controller.rb
+++ movie_app/app/controllers/subjects_controller.rb
@@ -70,6 +70,6 @@ class SubjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def subject_params
-      params.require(:subject).permit(:name, :synopsis, :year, :genre)
+      params.require(:subject).permit(:name, :synopsis, :year, :genre, :image)
     end
 end
--- movie_app/db/schema.rb
+++ movie_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611023632) do
+ActiveRecord::Schema.define(version: 20150611070041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,18 @@ ActiveRecord::Schema.define(version: 20150611023632) do
     t.date     "year"
     t.string   "genre"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
     t.text     "bio"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
